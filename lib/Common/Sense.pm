c3a5b6a85834e7cca414c6c97dd1eeb270ac050e
Adam Taylor <ajct@cpan.org>
2011-01-20 00:36:41 +0000

    Import of AJCT/Common-Sense-AJCT-0.01 from CPAN.
    gitpan-cpan-distribution: Common-Sense-AJCT
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         AJCT/Common-Sense-0.01.tar.gz
    gitpan-cpan-author:       AJCT
    gitpan-cpan-maturity:     released

---
+++ lib/Common/Sense.pm
@@ -0,0 +1,51 @@
+# ABSTRACT: Like Modern::Perl but more amusing to use
+package Common::Sense;
+
+use Modern::Perl;
+
+sub import {
+    Modern::Perl->import();
+}
+
+
+1;
+
+__END__
+=pod
+
+=head1 NAME
+
+Common::Sense - Like Modern::Perl but more amusing to use
+
+=head1 VERSION
+
+version 0.01
+
+=head1 SYNOPSIS
+
+    use Common::Sense
+
+=head1 DESCRIPTION
+
+See L<Modern::Perl>.
+
+A redditor suggested 'use Common::Sense' and I thought it was more amusing than
+use Modern::Perl.
+
+http://www.reddit.com/r/perl/comments/f4e14/please_share_your_modern_perl_boilerplate/c1d7q0r
+
+There's no point actually using this... :)
+
+=head1 AUTHOR
+
+Adam Taylor <ajct@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2011 by Adam Taylor.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
